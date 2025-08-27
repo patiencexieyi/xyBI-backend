5cd4c6bb5d751b2f3f7ad8382c14dba18564d5b7
xieyiw@yonyou.com <xieyiw@yonyou.com>
2025-08-27 09:58:31 +0800

    删除多余注解

--- src/main/java/com/xybi/springbootinit/service/impl/PostServiceImpl.java
+++ src/main/java/com/xybi/springbootinit/service/impl/PostServiceImpl.java
@@ -21,6 +21,7 @@ import com.xybi.springbootinit.model.vo.UserVO;
 import com.xybi.springbootinit.service.PostService;
 import com.xybi.springbootinit.service.UserService;
 import com.xybi.springbootinit.utils.SqlUtils;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,6 +30,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+
 import lombok.extern.slf4j.Slf4j;
 import cn.hutool.core.collection.CollUtil;
 import org.apache.commons.lang3.ObjectUtils;
